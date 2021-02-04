a4c0d4d19760a32a76f4bb10380df86b91218a09
unknown <wallace.dantunes@gmail.com>
2021-02-04 02:15:03 -0300

    ajustando tela de sign in

--- src/UI/SignIn/index.tsx
+++ src/UI/SignIn/index.tsx
@@ -1,42 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button, Image } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom';
+import Carousel from '../../components/Carousel'
 
 function SignIn() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorEmail, setErrorEmail] = useState('')
+    const [errorPassword, setErrorPassword] = useState('')
+    const history = useHistory()
+    const login = () => {
+        if (email === '') {
+            setErrorEmail('*Este campo não pode ser vazio')
+        } else if (email !== '' && email !== null) {
+            if (!email.includes('@')) {
+                setErrorEmail('*O e-mail está incorreto')
+            } else {
+                setErrorEmail('')
+            }
+        }
+        if(password === '') {
+            setErrorPassword('*Este campo não pode ser vazio')
+        } else {
+            setErrorPassword('')
+        }
+    };
     return (
-        <Container>
+        <Container fluid>
             <Row>
-                <Col md={6}>Carousel</Col>
-                <Col md={6}>
+                <Col md={6} className="carousel-class hidden-sm"><Carousel /></Col>
+                <Col xs={12} sm={12} md={6}>
                     <Row className="input-invision">
                         <Col className="text-right invision-title"><b>Invision</b></Col>
                     </Row>
-                    <Row className="subtitle-margin">
+                    <Row className="justify-content-center subtitle-margin">
                         <Col className="text-center invision-subtitle">Welcome to Invision</Col>
                     </Row>
-                    <Row className="justify-content-md-center">
-                        <Col md={10}>
+                    <Row className="justify-content-center">
+                        <Col xs={10} sm={10} md={10}>
                             <Form>
-                                <Form.Group controlId="formBasicEmail">
+                                <span className="error">{errorEmail}</span>
+                                <Form.Group>
                                     <Form.Label className="label-input">Users name or Email</Form.Label>
-                                    <Form.Control type="text" placeholder="Users name or Email" />
+                                    <Form.Control
+                                        className={
+                                            errorEmail === '' ? '' : 'errorInput'
+                                        }
+                                        type="text"
+                                        value={email}
+                                        onChange={(ev) => setEmail(ev.target.value)}
+                                        placeholder="Users name or Email"
+                                    />
                                 </Form.Group>
-                                <Form.Group controlId="formBasicEmail">
+                                <span className="error">{errorPassword}</span>
+                                <Form.Group>
                                     <Form.Label className="label-input">Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" />
+                                    <Form.Control
+                                        className={
+                                            errorPassword === '' ? '' : 'errorInput'
+                                        }
+                                        type="password"
+                                        value={password}
+                                        onChange={(ev) => setPassword(ev.target.value)}
+                                        placeholder="Password"
+                                    />
                                     <Form.Text className="text-right cursor">Forgot password?</Form.Text>
                                 </Form.Group>
                             </Form>
                         </Col>
                     </Row>
-                    <Row className="justify-content-md-center">
-                        <Button className="btn-confirm">Sign in</Button>
+                    <Row className="justify-content-center">
+                        <Button className="btn-confirm" onClick={() => login()}>Sign in</Button>
                     </Row>
                     <hr className="hr-text" data-content="OR" />
-                    <Row className="justify-content-md-center">
+                    <Row className="justify-content-center">
                         <Button className="btn-google">
                             <Image
                                 className="img-google"
-                                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+                                src="/img/logo-google.png"
                                 width="30px"
                                 height="30px"
                             />
@@ -44,9 +85,10 @@ function SignIn() {
                         </Button>
                     </Row>
                     <br />
-                    <Row>
-                        <Col className="text-center invision-text">New <b>Invision</b>? <span className="blue-link">Create Account</span></Col>
+                    <Row className="justify-content-center">
+                        <Col className="text-center invision-text">New <b>Invision</b>? <span className="blue-link" onClick={() => history.push('/create')}>Create Account</span></Col>
                     </Row>
+                    <br />
                 </Col>
             </Row>
         </Container>
